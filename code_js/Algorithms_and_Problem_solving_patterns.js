723d44e2819f34b705cc19eda052aa8e38f2eb3b
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-08-17 12:52:12 +0530

    Add files via upload

--- code_js/Algorithms_and_Problem_solving_patterns.js
+++ code_js/Algorithms_and_Problem_solving_patterns.js
@@ -103,7 +103,7 @@ function same(arr1, arr2){
 // Time Complexity - N^2
 // Space Complexity - N
 
-console.log(same([1, 2, 3], [4, 1, 9]));
+// console.log(same([1, 2, 3], [4, 1, 9]));
 
 // REFACTORED
 function same1(arr1, arr2){
@@ -131,4 +131,43 @@ function same1(arr1, arr2){
 }
 // Time Complexity - N
 // Space Complexity - N
-console.log(same1([1, 2, 3], [4, 1,9]));
+// console.log(same1([1, 2, 3], [4, 1,9]));
+
+
+//--------------------------------------------------------------------
+// ANAGRAMS
+//Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+// validAnagram('', ''); // true
+// validAnagram('aaz', 'zza'); // false
+// validAnagram('anagram', 'nagaram'); // true
+// validAnagram('rat', 'car');   // false
+// validAnagram('awesome', 'awesom'); // false
+// validAnagram('qwerty', 'qeywrt');   // true
+// validAnagram('texttwisttime', 'timetwisttext'); // true
+
+function validAnagram(first, second){
+  if(first.length !== second.length) return false;
+  const lookup = {};
+
+  for(let i = 0; i < first.length; i++){
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+
+  for(let i = 0; i < second.length; i++){
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    // if letter exists, decrement, otherwise return false
+    if(!lookup[letter]){
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+// Time Complexity: N
+// Space Complexity: N
+console.log(validAnagram('aaz', 'zza')); // false
+console.log(validAnagram('anagram', 'nagaram')); // true
