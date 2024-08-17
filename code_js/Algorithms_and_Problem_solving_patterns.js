598095f5c3e6d6a97d91615d9b701134a8665ef0
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-08-17 15:49:50 +0530

    Add files via upload

--- code_js/Algorithms_and_Problem_solving_patterns.js
+++ code_js/Algorithms_and_Problem_solving_patterns.js
@@ -236,3 +236,19 @@ function sumZero1(arr){
 // countUniqeValues([]); // 0
 // countUniqeValues([-2, -1, -1, 0, 1]);  // 4
 
+function countUniqeValues(arr){
+  if(arr.length === 0) return 0;
+  var i = 0;
+  for(var j = 1; j < arr.length; j++){
+    if(arr[i] !== arr[j]){
+      i++;
+      arr[i] = arr[j];
+    }
+    // console.log(i, j);  // this will show the position of i and j
+  }
+  return i + 1;
+}
+// Time complexity: O(N)
+// Space complexity: O(1)
+console.log(countUniqeValues([1, 1, 1, 1,2]));
+console.log(countUniqeValues([1, 2, 3, 3, 5, 5, 8, 9]));
