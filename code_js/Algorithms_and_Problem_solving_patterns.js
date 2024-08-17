d00b75de04e76bec16cb3a4e14d04734b9848e4f
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-08-17 13:43:37 +0530

    Add files via upload

--- code_js/Algorithms_and_Problem_solving_patterns.js
+++ code_js/Algorithms_and_Problem_solving_patterns.js
@@ -169,5 +169,70 @@ function validAnagram(first, second){
 }
 // Time Complexity: N
 // Space Complexity: N
-console.log(validAnagram('aaz', 'zza')); // false
-console.log(validAnagram('anagram', 'nagaram')); // true
+// console.log(validAnagram('aaz', 'zza')); // false
+// console.log(validAnagram('anagram', 'nagaram')); // true
+
+
+//---------------------------------------------------------------
+// MULTIPLE POINTERS
+// creating pointers or values that correspond to an 
+// index or position and move towards the beginnig, 
+// end or middle based on a certain condition
+
+// Very efficient for solving problems 
+// with minimal space complexity as well
+
+// Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+
+// sumZero([-3, -2, -1, 0, 1, 2, 3]);    // [-3, 3]
+// sumZero([-2, 0, 1, 3]);   // undefined
+// sumZero([1, 2, 3]);   // undefined
+
+// NAIV SOLUTION
+function sumZero(arr){
+  for(let i=0; i<arr.length; i++){
+    for(let j=i+1; j < arr.length; j++){
+      if(arr[i] + arr[j] === 0){
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+// console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+// console.log(sumZero([-2, 0, 1, 3]));
+
+
+// REFACTOR
+function sumZero1(arr){
+  let left = 0;
+  let right = arr.length -1;
+  while(left < right){
+    let sum = arr[left] + arr[right];
+    if(sum === 0){
+      return [arr[left], arr[right]];
+    } else if(sum > 0){
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// console.log(sumZero1([-3, -2, -1, 0, 1, 2, 3]));
+// console.log(sumZero1([-2, 0, 1, 3]));
+
+
+
+//----------------------------------------------------------------
+
+// CountUniqueValues
+//Implement a function called countUniqueValues, hich accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+// countUniqeValues([1, 1, 1, 1, 1, 2]);  // 2
+// countUniqeValues([1, 2, 3,4, 4, 4, 7, 7, 12, 12, 13]); // 7
+// countUniqeValues([]); // 0
+// countUniqeValues([-2, -1, -1, 0, 1]);  // 4
+
